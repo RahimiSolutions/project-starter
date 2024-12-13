6b1c8ea42e06cb0a0a59ce306bd4fd69a3e4877d
RahimiSolutions <rahimisolutions@gmail.com>
2024-12-13 21:50:50 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Lenis from 'lenis';
+	import 'lenis/dist/lenis.css';
+	import { onDestroy, onMount } from 'svelte';
+
+
+    let lenis: Lenis;
+
+    onMount(() => {
+
+		lenis = new Lenis({
+			duration: 1.3,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+			touchMultiplier: 2,
+			infinite: false
+		});
+
+		function raf(time: any) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
+
+	onDestroy(() => {
+		if (lenis) {
+			lenis.destroy();
+		}
+	});
+</script>
+
+<h1>Starter Project</h1>
+
+
+<style lang="scss">
+    
+</style>
